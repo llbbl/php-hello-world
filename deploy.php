6b62624b9b4c1e3af8615adc126e80f92b6ae74a
Logan Lindquist <logan@llbbl.com>
2020-10-04 21:38:31 -0500

    lets see if runs on self hosted

---
+++ deploy.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Project name
+set('application', 'php-hello-world');
+
+// Project repository
+set('repository', 'git@github.com:llbbl/php-hello-world.git');
+
+host('php-hello-world')
+    ->hostname('34.218.232.136')
+    ->user('forge')
+    ->port('22')
+    ->identityFile('~/.ssh/id_ed25519')
+    ->forwardAgent(true)
+    ->multiplexing(true)
+    ->addSshOption('UserKnownHostsFile', '/dev/null')
+    ->addSshOption('StrictHostKeyChecking', 'no')
+    ->set('deploy_path', '~/{{application}}');
+
+
+add('shared_dirs', []);
+
+set('writable_mode', 'chmod');
+
+// Writable dirs by web server
+add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+
